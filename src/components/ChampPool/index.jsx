4ac6da545778fab11bc6f0cf77de43bebdfb4048
Carlos Silva <silvacj90@gmail.com>
2024-02-19 00:30:39 -0500

    incorporate champ search

--- src/components/ChampPool/index.jsx
+++ src/components/ChampPool/index.jsx
@@ -1,8 +1,32 @@
+import { useState, useEffect } from "react";
 import champions from "../../data/champion.json";
 import styles from "./ChampPool.module.css";
 const BASE_URL = "https://ddragon.leagueoflegends.com/cdn/14.3.1/img/champion/";
 
 const ChampPool = () => {
+  const [originalChampPool, setOriginalChampPool] = useState([]);
+  const [filteredChampPool, setFilteredChampPool] = useState([]);
+
+  useEffect(() => {
+    const objToArr = () => {
+      const arr = Object.entries(champions.data).map((v) => v[1]);
+      setOriginalChampPool(arr);
+      setFilteredChampPool(arr);
+    };
+    objToArr();
+  }, []);
+
+  const sortPool = (searchText) => {
+    if (searchText.trim() === "") {
+      setFilteredChampPool(originalChampPool);
+    } else {
+      const updatedPool = originalChampPool.filter((obj) =>
+        obj.name.toLowerCase().includes(searchText.toLowerCase())
+      );
+      setFilteredChampPool(updatedPool);
+    }
+  };
+
   return (
     <div>
       <h2>Champions</h2>
@@ -11,8 +35,30 @@ const ChampPool = () => {
       <button>Bot</button>
       <button>Sup</button>
       <button>Jng</button>
+      <form>
+        <label htmlFor="search">
+          Search:
+          <input
+            type="text"
+            id="search"
+            name="search"
+            onChange={(e) => sortPool(e.target.value)}
+          />
+        </label>
+      </form>
       <div className={styles.championsContainer}>
-        {Object.entries(champions.data).map(([championId, championObj]) => {
+        {
+          filteredChampPool.map((championObj) => {
+            return (
+              <div key={championObj.id}>
+                <img
+                  src={`${BASE_URL}${championObj.image.full}`}
+                  alt={championObj.name}
+                />
+              </div>
+            );
+          })
+          /* {Object.entries(champions.data).map(([championId, championObj]) => {
           return (
             <div key={championId}>
               <img
@@ -21,7 +67,8 @@ const ChampPool = () => {
               />
             </div>
           );
-        })}
+        })} */
+        }
       </div>
     </div>
   );
