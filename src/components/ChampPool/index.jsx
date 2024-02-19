4a6cdda79973330fa364b33fd07442f27f1a68af
Carlos Silva <silvacj90@gmail.com>
2024-02-19 00:52:23 -0500

    add comment

--- src/components/ChampPool/index.jsx
+++ src/components/ChampPool/index.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import champions from "../../data/champion.json";
 import styles from "./ChampPool.module.css";
 const BASE_URL = "https://ddragon.leagueoflegends.com/cdn/14.3.1/img/champion/";
-
+//comment
 const ChampPool = () => {
   const [originalChampPool, setOriginalChampPool] = useState([]);
   const [filteredChampPool, setFilteredChampPool] = useState([]);
